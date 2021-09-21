797c6919fc2264507552c353524b7503702cc342
Patricia Faustino de Brito de Jesus <patricia.faustino@iteris.com.br>
2021-09-20 23:16:19 -0300

    adicionando metodos http

--- src/index.js
+++ src/index.js
@@ -1,14 +1,44 @@
 //importando express
+const { response, request } = require('express');
 const express = require('express');
 
 //chamar função do express
 const app = express();
 
+//para podermos receber o json na aplicação
+
+app.use(express.json());
 /*/app.get("/controle", (request, response) => {
     nossa função
 })*/
-app.get("/", (request, response) => {
-    return response.json({message: "Hello World Ignite!"});
+
+app.get("/courses", (request, response) => {
+    const query = request.query;
+    console.log(query);
+    return response.json(
+        ["Curso 1", "Curso 2", "Curso 3"]
+    );
+});
+
+
+app.post("/courses", (request, response) => {
+    const body =  request.body;
+    console.log(body);
+    return response.json( ["Curso 1", "Curso 2", "Curso 3", "Curso 4"])
+});
+
+app.put("/courses/:id", (request, response) => {
+    const {id} = request.params;
+    console.log(id);
+    return response.json( ["Curso 6", "Curso 2", "Curso 3", "Curso 4"])
+});
+
+app.patch("/courses/:id", (request, response) => {
+    return response.json( ["Curso 6", "Curso 7", "Curso 3", "Curso 4"])
+});
+
+app.delete("/courses/:id", (request, response) => {
+    return response.json( ["Curso 6", "Curso 7", "Curso 4"])
 });
 
 //definir porta onde a nossa aplicação estará rodando
